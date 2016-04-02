b45080841e14b75aecfaee192318a204e81f389d
emrekap <emrekap@hotmail.com>
2016-04-02 16:40:53 +0300

    Added files via upload

---
+++ Model/GameObjects.js
@@ -0,0 +1,66 @@
+﻿var GameObjects = window.GameObjects || {};
+
+function shuffle(array) {
+    var currentIndex = 8;
+    console.log("array.length: " + array.length);
+    var temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        console.log("randomIndex: " + randomIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        console.log("Value: " + temporaryValue + " index: " + currentIndex + " temporary: " + temporaryValue );
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+        console.log("current value: " + array[currentIndex] + " random value: " + array[randomIndex]);
+    }
+
+    return array;
+}
+
+
+
+
+var Card = function (TheCardType) {
+    console.log("Called constructor with: " + TheCardType);
+    this.sprite = game.add.sprite(100, 100, 'cards');
+    this.cardsClosed = true;
+    this.Type = TheCardType;
+    this.sprite.animations.add('apple', [0,4]);
+    this.sprite.animations.add('avakado', [1,4]);
+    this.sprite.animations.add('banana', [2,4]);
+    this.sprite.animations.add('pumpkin', [3,4]);
+    this.sprite.animations.add('questionmark',[4]);
+
+    //this.sprite.animations.play(TheCardType);
+}   
+
+Card.prototype.GetType = function()
+{return this.Type;}
+  
+Card.prototype.GetSprite = function()
+{return this.sprite;}
+
+Card.prototype.GetType = function()
+{return Type;}
+
+Card.prototype.SetType= function (s) {
+    thisType = s;
+}
+
+Card.prototype.PositionCard= function (x, y)
+{
+    this.sprite.x = x;
+    this.sprite.y = y;
+}
+Card.prototype.ShowCard = function ()
+{
+    this.sprite.animations.play(this.Type);
+  
+}
---
+++ Screen/ScreenManager.js
@@ -0,0 +1,60 @@
+﻿var showLogs = true;
+if (showLogs) {
+    console.log("Inside ScreenManager");
+}
+var gameWidth = 800;
+var gameHeight = 450;
+var backgroundColor = "#000000";
+var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, 'KulturGameDev', {
+    preload: preload, create: create, update: update
+});
+
+
+var Cards = [];
+
+var CardTypes =[];
+CardTypes.push('apple','banana', 'avakado', 'pumpkin' );
+
+
+function preload() {
+    if (showLogs) {
+        console.log("ScreenManager-preload");
+    }
+   game.load.image('background', 'Pictures/background.png');
+
+  game.load.atlas('cards', 'Sprites/fruits.png', 'Sprites/fruits.json', Phaser.Loader.TEXTURE_ATLAS_JSON_ARRAY);
+}
+
+function create() {
+    if (showLogs)
+        console.log("ScreenManager ­ create");
+
+   var Background = game.add.sprite(game.world.centerX, game.world.centerY, 'background');
+   Background.anchor.setTo(0.5, 0.5);
+
+   for (var i = 0; i < (CardTypes.length); i++) {           //kartları arrayın içine atıyoruz
+       for (var j = 0; j < 2; j++) {
+           var NewCard = new Card(CardTypes[i]);
+           Cards.push(NewCard);
+       }
+
+       console.log("card " + i + " " + Cards[i]);
+   }
+
+   console.log("Shuffling");
+   shuffle(Cards);             //kartları karıştırıyoruz
+    
+   for (var row = 0; row < 2;row++) {
+       for (var iCurrentCard = 0; iCurrentCard < 4; iCurrentCard++)
+       {
+           Cards[iCurrentCard + (4 * row)].PositionCard((iCurrentCard + 1) * 150, (row + 1) * 150);
+           Cards[iCurrentCard + (4 * row)].ShowCard();
+       }
+   }
+
+}
+
+function update() {
+
+
+}
